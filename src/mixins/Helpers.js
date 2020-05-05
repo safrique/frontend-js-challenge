a9c090f7d498cc6f40321a0540906457b330e58c
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-05 20:57:38 +0100

    added try catch to Helpers.js

--- src/mixins/Helpers.js
+++ src/mixins/Helpers.js
@@ -1,39 +1,43 @@
 export default {
   getPeoplePropertyData (people, category) {
     let data = {}
-    // console.log(`${category} getPeoplePropertyData people:`, people)
-    let preferences = [`pet`, `fruit`]
-    let nested = []
-    let nested_category
+    try {
+      // console.log(`${category} getPeoplePropertyData people:`, people)
+      let preferences = [`pet`, `fruit`]
+      let nested = []
+      let nested_category
 
-    if (preferences.includes(category)) {
-      for (let i = 0, j = preferences.length; i < j; i++) { nested.push(preferences[i]) }
-      nested_category = `preferences`
-    }
-    // console.log(`${category} nested:`, nested)
+      if (preferences.includes(category)) {
+        for (let i = 0, j = preferences.length; i < j; i++) { nested.push(preferences[i]) }
+        nested_category = `preferences`
+      }
+      // console.log(`${category} nested:`, nested)
 
-    for (let person in people) {
-      if (people.hasOwnProperty(person)) {
-        // console.log(`person:${person} -- data`, people[person])
-        // console.log(`${category}:${people[person][category]} -- type=${typeof people[person][category]}`)
+      for (let person in people) {
+        if (people.hasOwnProperty(person)) {
+          // console.log(`person:${person} -- data`, people[person])
+          // console.log(`${category}:${people[person][category]} -- type=${typeof people[person][category]}`)
 
-        if (nested.includes(category)) {
-          // console.log(`person:${person} -- people[person][nested_category]`, people[person][nested_category])
-          if (data.hasOwnProperty(people[person][nested_category][category])) {
-            data[people[person][nested_category][category]]++
-            continue
+          if (nested.includes(category)) {
+            // console.log(`person:${person} -- people[person][nested_category]`, people[person][nested_category])
+            if (data.hasOwnProperty(people[person][nested_category][category])) {
+              data[people[person][nested_category][category]]++
+              continue
+            }
+
+            data[people[person][nested_category][category]] = 1
+          } else {
+            if (data.hasOwnProperty(people[person][category])) {
+              data[people[person][category]]++
+              continue
+            }
+
+            data[people[person][category]] = 1
           }
-
-          data[people[person][nested_category][category]] = 1
-        } else {
-          if (data.hasOwnProperty(people[person][category])) {
-            data[people[person][category]]++
-            continue
-          }
-
-          data[people[person][category]] = 1
         }
       }
+    } catch (e) {
+      console.log(`Helpers getPeoplePropertyData error...`, e)
     }
 
     // console.log(`getPeoplePropertyData data:`, data)
@@ -44,20 +48,24 @@ export default {
     let summary = {}
     // console.log(`getBracketsSummary data:`, data)
 
-    for (let item in data) {
-      if (data.hasOwnProperty(item)) {
-        let bracket = (category === `age` ? this.getNumberItemBracket(item) : item)
-        // console.log(`item=${item} -- data=${data[item]} -- bracket:${bracket} -- summary`, summary)
+    try {
+      for (let item in data) {
+        if (data.hasOwnProperty(item)) {
+          let bracket = (category === `age` ? this.getNumberItemBracket(item) : item)
+          // console.log(`item=${item} -- data=${data[item]} -- bracket:${bracket} -- summary`, summary)
 
-        if (summary.hasOwnProperty(bracket)) {
-          summary[bracket] += data[item]
-          // console.log(`summary after updating:`, summary)
-          continue
+          if (summary.hasOwnProperty(bracket)) {
+            summary[bracket] += data[item]
+            // console.log(`summary after updating:`, summary)
+            continue
+          }
+
+          summary[bracket] = data[item]
+          // console.log(`summary after insert:`, summary)
         }
-
-        summary[bracket] = data[item]
-        // console.log(`summary after insert:`, summary)
       }
+    } catch (e) {
+      console.log(`Helpers getBracketsSummary error...`, e)
     }
 
     // console.log(`item summary:`, summary)
@@ -65,10 +73,14 @@ export default {
   },
 
   getNumberItemBracket (item) {
-    item = parseInt(item)
-    let from_age = (Math.trunc(item / 10) * 10).toString()
-    let to_age = ((Math.trunc(item / 10) + 1) * 10 - 1).toString()
-    return from_age + ` to ` + to_age
+    try {
+      item = parseInt(item)
+      let from_age = (Math.trunc(item / 10) * 10).toString()
+      let to_age = ((Math.trunc(item / 10) + 1) * 10 - 1).toString()
+      return from_age + ` to ` + to_age
+    } catch (e) {
+      console.log(`Helpers getNumberItemBracket error...`, e)
+    }
   },
 
   buildPersonAttributes (people) {
@@ -78,37 +90,41 @@ export default {
       exclude = [`_id`, `name`, `longitude`, `latitude`,],
       person = people[0]
 
-    for (let item in person) {
-      if (person.hasOwnProperty(item)) {
-        if (nested.includes(item)) {
-          // console.log(`item ${item} is nested`)
-          for (let sub_item in person[item]) {
-            if (person[item].hasOwnProperty(sub_item)) {
-              // console.log(`sub_item ${sub_item} in person[item]=${person[item][sub_item]}`)
-              if (!exclude.includes(sub_item)) {
-                // console.log(`sub_item=${sub_item} included`)
-                data.push({
-                  value: sub_item,
-                  label: sub_item
-                })
-                // continue
+    try {
+      for (let item in person) {
+        if (person.hasOwnProperty(item)) {
+          if (nested.includes(item)) {
+            // console.log(`item ${item} is nested`)
+            for (let sub_item in person[item]) {
+              if (person[item].hasOwnProperty(sub_item)) {
+                // console.log(`sub_item ${sub_item} in person[item]=${person[item][sub_item]}`)
+                if (!exclude.includes(sub_item)) {
+                  // console.log(`sub_item=${sub_item} included`)
+                  data.push({
+                    value: sub_item,
+                    label: sub_item
+                  })
+                  // continue
+                }
+                // console.log(`sub_item=${sub_item} excluded`)
               }
-              // console.log(`sub_item=${sub_item} excluded`)
             }
+          } else {
+            // console.log(`item ${item} in person=${person[item]}`)
+            if (!exclude.includes(item)) {
+              // console.log(`item=${item} included`)
+              data.push({
+                value: item,
+                label: item
+              })
+              // continue
+            }
+            // console.log(`item=${item} excluded`)
           }
-        } else {
-          // console.log(`item ${item} in person=${person[item]}`)
-          if (!exclude.includes(item)) {
-            // console.log(`item=${item} included`)
-            data.push({
-              value: item,
-              label: item
-            })
-            // continue
-          }
-          // console.log(`item=${item} excluded`)
         }
       }
+    } catch (e) {
+      console.log(`Helpers buildPersonAttributes error...`, e)
     }
 
     // console.log(`data:`, data)
