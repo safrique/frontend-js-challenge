e2800f852585eebebb8ef8753d698df1b818d0c1
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-05 21:36:25 +0100

    added try catch to summary.js

--- src/store/modules/data/summary.js
+++ src/store/modules/data/summary.js
@@ -7,23 +7,62 @@ const summary = {
 
 const mutations = {
   SET_DATA (state, payload) {
-    state.summary[`${payload.category}Data`] = Helpers.getPeoplePropertyData(payload.data, payload.category)
+    try {
+      state.summary[`${payload.category}Data`] = Helpers.getPeoplePropertyData(payload.data, payload.category)
+    } catch (e) {
+      console.log(`summary.js SET_DATA error...`, e)
+      return false
+    }
   },
 
   SET_BRACKETS_DATA (state, payload) {
-    state.summary[`${payload.category}BracketsData`] = Helpers.getBracketsSummary(payload.data, payload.category)
+    try {
+      state.summary[`${payload.category}BracketsData`] = Helpers.getBracketsSummary(payload.data, payload.category)
+    } catch (e) {
+      console.log(`summary.js SET_BRACKETS_DATA error...`, e)
+      return false
+    }
   },
 }
 
 const getters = {
-  getAgeData: (state) => { return ((_.isEmpty(state.summary.ageData)) ? [] : state.summary.ageData) },
-  // getAgeBracketsData: (state) => { return ((_.isEmpty(state.summary.ageBracketsData)) ? [] : state.summary.ageBracketsData) },
-  getAgeBracketsData: (state) => { return state.summary.ageBracketsData },
+  getAgeData: (state) => {
+    try {
+      return ((_.isEmpty(state.summary.ageData)) ? [] : state.summary.ageData)
+    } catch (e) {
+      console.log(`summary.js getAgeData error...`, e)
+      return false
+    }
+  },
+
+  getAgeBracketsData: (state) => {
+    try {
+      return state.summary.ageBracketsData
+    } catch (e) {
+      console.log(`summary.js getAgeBracketsData error...`, e)
+      return false
+    }
+  },
 }
 
 const actions = {
-  setData ({ commit, state }, payload) { commit('SET_DATA', payload) },
-  setBracketsData ({ commit, state }, payload) { commit('SET_BRACKETS_DATA', payload) },
+  setData ({ commit, state }, payload) {
+    try {
+      commit('SET_DATA', payload)
+    } catch (e) {
+      console.log(`summary.js setData error...`, e)
+      return false
+    }
+  },
+
+  setBracketsData ({ commit, state }, payload) {
+    try {
+      commit('SET_BRACKETS_DATA', payload)
+    } catch (e) {
+      console.log(`summary.js setBracketsData error...`, e)
+      return false
+    }
+  },
 }
 
 export default {
