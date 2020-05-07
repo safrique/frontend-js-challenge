d01a74947ea9cce0fb2cb01b750233b6dc9b0315
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-07 21:42:32 +0100

    moved Element UI CSS from index.html to App.vue

--- test/components/PieChart.test.js
+++ test/components/PieChart.test.js
@@ -1,36 +1,23 @@
-// import { shallowMount } from '@vue/test-utils'
-
+import { shallowMount } from '@vue/test-utils'
 import PieChart from '../../src/components/charts/PieChart'
 import PieChartTable from '../../src/components/data/ChartTable'
 
-// Trying to use ElementUI customer elements - TODO: try find a fix for not recognising them
-import { shallowMount, createLocalVue } from '@vue/test-utils'
-import ElementUI from 'element-ui'
-const localVue = createLocalVue()
-localVue.use(ElementUI)
+// // Trying to use ElementUI customer elements - TODO: try find a fix for not recognising them
+// import { shallowMount, createLocalVue } from '@vue/test-utils'
+// import ElementUI from 'element-ui'
+// const localVue = createLocalVue()
+// localVue.use(ElementUI)
 
 const wrapper = shallowMount(PieChart)
 
 describe(`PieChart`, () => {
-  it('PieChart renders a vue instance', () => {
-    expect(wrapper.isVueInstance()).toBe(true)
-  })
+  it('PieChart renders a vue instance', () => {expect(wrapper.isVueInstance()).toBe(true)})
 
-  it(`initial value=age, i.e. type of data displayed`, () => {
-    expect(wrapper.vm.$data.value).toBe(`age`)
-  })
+  it(`initial value=age, i.e. type of data displayed`, () => {expect(wrapper.vm.$data.value).toBe(`age`)})
 
-  it(`Contains the PieChartTable component`, () => {
-    expect(wrapper.contains(PieChartTable)).toBe(true)
-  })
+  it(`Contains the PieChartTable component`, () => {expect(wrapper.contains(PieChartTable)).toBe(true)})
 
-  it(`Contains the chart containing div`, () => {
-    expect(wrapper.contains('div#pie-chart-render')).toBe(true)
-  })
+  it(`Contains the chart containing div`, () => {expect(wrapper.contains('div#pie-chart-render')).toBe(true)})
 
-  it(`Contains the dropdown with other options of data types to display`, () => {
-    expect(wrapper.contains('el-select')).toBe(true)
-  })
-
-  // it(``, (wrapper.) => {expect().toBe()})
+  it(`Contains the dropdown with other options of data types to display`, () => {expect(wrapper.contains('el-select')).toBe(true)})
 })
