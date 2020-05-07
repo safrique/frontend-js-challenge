680ed43c38b07efc6bf63965502f6e7a975bca88
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-07 12:43:47 +0100

    testing PieChart.vue using PieChart.test.js

--- src/test/components/PieChart.test.js
+++ src/test/components/PieChart.test.js
@@ -1,16 +1,37 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import ElementUI from 'element-ui'
+
 import PieChart from '../../components/charts/PieChart'
+import PieChartTable from '../../components/data/ChartTable'
+
+// Trying to use ElementUI customer elements - TODO: try find a fix for not recognising them
+const localVue = createLocalVue()
+localVue.use(ElementUI)
 
 const wrapper = shallowMount(PieChart)
 
 describe(`PieChart`, () => {
-  // // Inspect the raw component options
-  // it('has a created hook', () => {
-  //   expect(typeof PieChart.checkDataBuildChart).toBe('function')
-  // })
+  // Check it renders as a Vue instance
+  it('PieChart renders a vue instance', () => {
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
 
-  // Inspect the component instance on mount
+  // Test the initial type of data displayed
   it(`initial value=age`, () => {
     expect(wrapper.vm.$data.value).toBe(`age`)
   })
+
+  it(`Contains the PieChartTable component`, () => {
+    expect(wrapper.contains(PieChartTable)).toBe(true)
+  })
+
+  it(`Contains the chart containing div`, () => {
+    expect(wrapper.contains('div#pie-chart-render')).toBe(true)
+  })
+
+  it(`Contains the dropdown with other options of data types to display`, () => {
+    expect(wrapper.contains('el-select')).toBe(true)
+  })
+
+  // it(``, (wrapper.) => {expect().toBe()})
 })
