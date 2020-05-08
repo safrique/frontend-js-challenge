bc2c06ca3ea6f8b29957fddf9c82ddf33d824d69
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-08 18:15:14 +0100

    removed unused components on instructions tab

--- src/components/usability/AppInstructions.vue
+++ src/components/usability/AppInstructions.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="app-instructions">
     <h1>App Instructions</h1>
-    <TheInstructions></TheInstructions>
-<!--    <DownloadInstructions></DownloadInstructions>-->
-    <hr>
-    <VueLinks></VueLinks>
+    <ol>
+      <li>#1</li>
+      <li>#2</li>
+      <li>#3</li>
+    </ol>
   </div>
 </template>
 
 <script>
-  import VueLinks from '../vue-eco/VueLinks'
-  import TheInstructions from './TheInstructions'
-  import DownloadInstructions from './DownloadInstructions'
-
   export default {
     name: 'AppInstructions',
-
-    components: {
-      VueLinks,
-      TheInstructions,
-      DownloadInstructions,
-    }
   }
 </script>
 
